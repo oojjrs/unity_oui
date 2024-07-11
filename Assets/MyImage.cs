c45e63d84ef60396cec84b169098fd6975705db6
oojjrs <oojjrs@gmail.com>
2024-07-11 22:21:16 +0900

    MyImage 추가

---
+++ Assets/MyImage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace oojjrs.oui
+{
+    [RequireComponent(typeof(Image))]
+    public class MyImage : MonoBehaviour
+    {
+        public Sprite Sprite
+        {
+            get => GetComponent<Image>().sprite;
+            set => GetComponent<Image>().sprite = value;
+        }
+    }
+}
