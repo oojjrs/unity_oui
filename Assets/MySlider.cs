42eb03ad8eca998cd87c2f26e01b0bb1ea58fd1c
oojjrs <oojjrs@gmail.com>
2024-08-12 10:41:08 +0900

    MySlider 추가

---
+++ Assets/MySlider.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace oojjrs.oui
+{
+    [RequireComponent(typeof(Slider))]
+    public class MySlider : MonoBehaviour
+    {
+        public interface CallbackInterface
+        {
+            float InitialValue { get; }
+
+            void OnValueChanged(float value);
+        }
+
+        [SerializeField]
+        private MyText _text;
+
+        private CallbackInterface Callback { get; set; }
+        public string Text
+        {
+            get => (_text != default) ? _text.Text : string.Empty;
+            set
+            {
+                if (_text != default)
+                    _text.Text = value;
+            }
+        }
+        public float Value
+        {
+            get => GetComponent<Slider>().value;
+            set => GetComponent<Slider>().value = value;
+        }
+
+        private void OnEnable()
+        {
+            if (Callback != default)
+                GetComponent<Slider>().value = Callback.InitialValue;
+        }
+
+        private void Start()
+        {
+            Callback = GetComponent<CallbackInterface>();
+            if (Callback == default)
+                Debug.LogWarning($"{name}> DON'T HAVE CALLBACK FUNCTION.");
+
+            if (Callback != default)
+                GetComponent<Slider>().value = Callback.InitialValue;
+        }
+
+        public void OnValueChanged(float value)
+        {
+            Callback?.OnValueChanged(value);
+        }
+    }
+}
