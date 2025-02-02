17cfddfea9ae5534ec0dc1de91d2c0a147cb9ca5
oojjrs <oojjrs@gmail.com>
2025-02-02 13:36:45 +0900

    MySwapper 추가

---
+++ Assets/MySwapper.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+namespace oojjrs.oui
+{
+    [RequireComponent(typeof(MySelector))]
+    public class MySwapper : MonoBehaviour, MySelector.CallbackInterface
+    {
+        public interface CallbackInterface
+        {
+            bool Interactable { get; }
+            bool IsOn { get; }
+
+            void OnClick(bool isOn);
+        }
+
+        private CallbackInterface Callback { get; set; }
+
+        private void Awake()
+        {
+            Callback = GetComponent<CallbackInterface>();
+        }
+
+        private void Start()
+        {
+            if (Callback == default)
+                Debug.LogWarning($"{name}> DON'T HAVE CALLBACK FUNCTION.");
+        }
+
+        int MySelector.CallbackInterface.GetIndex()
+        {
+            if (Callback != default)
+            {
+                if (Callback.Interactable)
+                {
+                    if (Callback.IsOn)
+                        return 1;
+                    else
+                        return 0;
+                }
+                else
+                {
+                    if (Callback.IsOn)
+                        return 3;
+                    else
+                        return 2;
+                }
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public void OuiClick()
+        {
+            if (Callback != default)
+            {
+                if (Callback.Interactable)
+                    Callback.OnClick(Callback.IsOn == false);
+            }
+
+            GetComponent<MySelector>().OuiUpdate();
+        }
+    }
+}
---
+++ Assets/MySwapperButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace oojjrs.oui
+{
+    [RequireComponent(typeof(MyButton))]
+    public class MySwapperButton : MonoBehaviour, MyButton.CallbackInterface
+    {
+        void MyButton.CallbackInterface.OnClick()
+        {
+            GetComponent<MyButton>().ClickSound = MyButton.ClickSoundEnum.Switch;
+            GetComponentInParent<MySwapper>().OuiClick();
+        }
+    }
+}
