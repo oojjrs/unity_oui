b616d7443508fa4e553a8a7367237a03b5fefbe6
oojjrs <oojjrs@blackstormcompany.com>
2025-06-19 16:33:57 +0900

    MySelectable 추가

---
+++ Assets/MySelectable.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Assets
+{
+    [RequireComponent(typeof(Selectable))]
+    public class MySelectable : MonoBehaviour, IDeselectHandler, ISelectHandler
+    {
+        public interface CallbackInterface
+        {
+            void OnDeselect();
+            void OnSelect();
+        }
+
+        private CallbackInterface[] Callbacks { get; set; }
+        public bool Interactable
+        {
+            get => GetComponent<Selectable>().interactable;
+            set => GetComponent<Selectable>().interactable = value;
+        }
+
+        private void Awake()
+        {
+            Callbacks = GetComponents<CallbackInterface>();
+        }
+
+        private void Start()
+        {
+            if (Callbacks == default)
+                Debug.LogWarning($"{name}> DON'T HAVE CALLBACK FUNCTION.");
+        }
+
+        void IDeselectHandler.OnDeselect(BaseEventData eventData)
+        {
+            if (Interactable && (Callbacks != default))
+            {
+                foreach (var callback in Callbacks)
+                    callback.OnDeselect();
+            }
+        }
+
+        void ISelectHandler.OnSelect(BaseEventData eventData)
+        {
+            if (Interactable && (Callbacks != default))
+            {
+                foreach (var callback in Callbacks)
+                    callback.OnSelect();
+            }
+        }
+    }
+}
