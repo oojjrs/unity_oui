d12d2a7c3ed62593a23c7890a01a98e56dbc9f35
oojjrs <oojjrs@gmail.com>
2026-03-21 14:19:41 +0900

    MyAsker> Arguments 구조 추가

--- Assets/MyAsker.cs
+++ Assets/MyAsker.cs
@@ -1,10 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace oojjrs.oui
 {
     public partial class MyAsker : MonoBehaviour
     {
+        public class MyAskerArguments
+        {
+            private Dictionary<string, object> NamedValues { get; } = new();
+            private Dictionary<Type, object> TypedValues { get; } = new();
+
+            public T Get<T>()
+            {
+                if (TypedValues.TryGetValue(typeof(T), out var value))
+                    return (T)value;
+                else
+                    return default;
+            }
+
+            public T Get<T>(string key)
+            {
+                if (NamedValues.TryGetValue(key, out var value))
+                    return (T)value;
+                else
+                    return default;
+            }
+
+            public void Set(object value)
+            {
+                TypedValues[value.GetType()] = value;
+            }
+
+            public void Set(string key, object value)
+            {
+                NamedValues[key] = value;
+            }
+        }
+
+        private MyAskerArguments _arguments;
+        private readonly MyAskerArguments _emptyArguments = new();
+
+        public MyAskerArguments Arguments => _arguments ?? _emptyArguments;
+
         private event Action OnNo;
         private event Action OnOk;
         private event Action OnYes;
@@ -35,20 +73,24 @@ namespace oojjrs.oui
             gameObject.SetActive(false);
         }
 
-        public void OuiOpenOk(Action onOk)
+        public void OuiOpenOk(MyAskerArguments arguments, Action onOk)
         {
             if (gameObject == default)
                 return;
+
+            _arguments = arguments;
 
             OnOk = onOk;
 
             gameObject.SetActive(true);
         }
 
-        public void OuiOpenYesNo(Action onYes, Action onNo)
+        public void OuiOpenYesNo(MyAskerArguments arguments, Action onYes, Action onNo)
         {
             if (gameObject == default)
                 return;
+
+            _arguments = arguments;
 
             OnNo = onNo;
             OnYes = onYes;
