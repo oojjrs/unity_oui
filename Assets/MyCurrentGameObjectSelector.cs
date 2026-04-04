e247df57f4947fcd1d7f82b27466ac220023cac1
oojjrs <oojjrs@gmail.com>
2026-04-04 23:38:06 +0900

    MyCurrentGameObjectSelector 추가

---
+++ Assets/MyCurrentGameObjectSelector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace oojjrs.oui
+{
+    public class MyCurrentGameObjectSelector : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            EventSystem.current.SetSelectedGameObject(gameObject);
+        }
+    }
+}
