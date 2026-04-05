791d99576c75f974e920e47c03cdd9ddc429b824
oojjrs <oojjrs@gmail.com>
2026-04-05 19:36:18 +0900

    MyCurrentGameObjectSelector> 닫힐 때 처리도 추가함

--- Assets/MyCurrentGameObjectSelector.cs
+++ Assets/MyCurrentGameObjectSelector.cs
@@ -5,8 +5,18 @@ namespace oojjrs.oui
 {
     public class MyCurrentGameObjectSelector : MonoBehaviour
     {
+        private GameObject _previous;
+
+        private void OnDisable()
+        {
+            if ((_previous != default) && _previous.activeInHierarchy)
+                EventSystem.current.SetSelectedGameObject(_previous);
+        }
+
         private void OnEnable()
         {
+            _previous = EventSystem.current.currentSelectedGameObject;
+
             EventSystem.current.SetSelectedGameObject(gameObject);
         }
     }
