a718cd5773dfea349076e182845ed98f4f4e6dfb
oojjrs <oojjrs@gmail.com>
2026-08-18 21:47:56 +0900

    MyText 정돈

--- Packages/src/Runtime/MyText.cs
+++ Packages/src/Runtime/MyText.cs
@@ -18,13 +18,33 @@ namespace oojjrs.oui
             set => GetComponent<Text>().color = value;
         }
         public float PreferredHeight => GetComponent<Text>().preferredHeight;
+        public float PreferredWidth => GetComponent<Text>().preferredWidth;
         public string Text
         {
             get => GetComponent<Text>().text;
             set
             {
                 GetComponent<Text>().text = value;
-                ResizeToPreferredSize();
+
+                if (_autoHeight || _autoWidth)
+                {
+                    var text = GetComponent<Text>();
+                    var size = text.rectTransform.rect.size;
+
+                    if (_autoWidth)
+                        size.x = text.preferredWidth;
+
+                    if (_autoHeight)
+                    {
+                        if (_autoWidth)
+                            size.y = text.cachedTextGeneratorForLayout.GetPreferredHeight(text.text, text.GetGenerationSettings(new Vector2(size.x, 0))) / text.pixelsPerUnit;
+                        else
+                            size.y = text.preferredHeight;
+                    }
+
+                    text.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size.x);
+                    text.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, size.y);
+                }
             }
         }
         public int TextFromInt32
@@ -33,34 +53,6 @@ namespace oojjrs.oui
             {
                 Text = value.ToString();
             }
-        }
-
-        public Vector2 CalculatePreferredSize()
-        {
-            var text = GetComponent<Text>();
-            var size = text.rectTransform.rect.size;
-
-            if (_autoWidth)
-                size.x = text.preferredWidth;
-            if (_autoHeight)
-            {
-                if (_autoWidth)
-                    size.y = text.cachedTextGeneratorForLayout.GetPreferredHeight(text.text, text.GetGenerationSettings(new Vector2(size.x, 0))) / text.pixelsPerUnit;
-                else
-                    size.y = text.preferredHeight;
-            }
-            return size;
-        }
-
-        public void ResizeToPreferredSize()
-        {
-            var text = GetComponent<Text>();
-            var size = CalculatePreferredSize();
-
-            if (_autoWidth)
-                text.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size.x);
-            if (_autoHeight)
-                text.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, size.y);
         }
     }
 }
